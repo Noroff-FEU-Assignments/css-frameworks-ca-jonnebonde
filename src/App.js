35301d6d97607a89a72f48a182a8761cc2ab9d72
jonnebonde <jonnebonde@gmail.com>
2023-02-13 15:29:37 +0100

    initial commit and made the routing.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/navbar/Navbar";
+import Home  from "./components/pages/home/Home";
+import News from "./components/pages/news/News";
+import Contact from "./components/pages/contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+    <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/news' element={<News />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link href="#news">
+              <Link to="/news">News</Link>
+            </Nav.Link>
+            <Nav.Link href="#contact">
+              <Link to="/contact">Contact</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
