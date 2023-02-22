6644d12eac4b8b860247ba2ca64144f692ef6367
jonnebonde <jonnebonde@gmail.com>
2023-02-22 21:36:07 +0100

    made array of the newsimages

---
+++ src/assets/news/NewsImages.js
@@ -0,0 +1,61 @@
+import news1 from "./news-1.jpg";
+import news2 from "./news-2.jpg";
+import news3 from "./news-3.jpg";
+import news4 from "./news-4.jpg";
+import news5 from "./news-5.jpg";
+import news6 from "./news-6.jpg";
+import news7 from "./news-7.jpg";
+import news8 from "./news-8.jpg";
+
+const newsImages = [
+  {
+    src: news1,
+    alt: "News 1",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news2,
+    alt: "News 2",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news3,
+    alt: "News 3",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news4,
+    alt: "News 4",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news5,
+    alt: "News 5",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news6,
+    alt: "News 6",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news7,
+    alt: "News 7",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  },
+  {
+    src: news8,
+    alt: "News 8",
+    title: "Nunc porttitor vel",
+    description: "Nunc malesuada eget est fringilla dapibus.",
+  }
+];
+
+export default newsImages
