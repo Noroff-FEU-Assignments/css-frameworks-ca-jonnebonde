65a7d260d6f46dbb77c28d3d5105cd32329c2747
jonnebonde <jonnebonde@gmail.com>
2023-02-24 16:19:19 +0100

    started styling cards

--- src/components/pages/news/newscards/Newscards.js
+++ src/components/pages/news/newscards/Newscards.js
@@ -7,26 +7,24 @@ import Col from 'react-bootstrap/Col';
 
 function Newscards() {
   return (
-  
-      <Row className='news__cards row-cols-xxl-4 row-cols-xl-3 row-cols-lg-3 row-cols-md-2 row-cols-sm-1'  >
-        {newsImages.map((card) => (
-          <Col className="mb-3">
-            <Card className='' >
-              <Card.Img variant="top" src={card.src} />
-              <Card.Body>
-                <Card.Title>{card.title}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">MORE</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+
+    <Row className='news__cards g-4' xxl={4} xl={3} md={2} sm={1}>
+      {newsImages.map((card) => (
+        <Col className="mb-3">
+          <Card className='' >
+            <Card.Img variant="top" src={card.src} />
+            <Card.Body>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Text>
+                {card.description}
+              </Card.Text>
+              <Button variant="primary">MORE</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
 export default Newscards
-
