97660c321e0addac19c11018fe15d35ce55ce55f
jonnebonde <jonnebonde@gmail.com>
2023-03-05 22:04:11 +0100

    optimizing newscard responsiveness

--- src/components/pages/news/newscards/Newscards.js
+++ src/components/pages/news/newscards/Newscards.js
@@ -7,20 +7,20 @@ import Col from "react-bootstrap/Col";
 
 function Newscards() {
   return (
-    <Row className="news__cards g-4" xxl={4} xl={3} md={2} sm={1} xs={1}>
-      {newsImages.map((card) => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={card.src} />
-            <Card.Body>
-              <Card.Title>{card.title}</Card.Title>
-              <Card.Text>{card.description}</Card.Text>
-              <Button variant="primary">MORE</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+      <Row className="g-4"  xxl={4} xl={3} md={2} sm={1} xs={1}>
+        {newsImages.map((card) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={card.src} />
+              <Card.Body>
+                <Card.Title>{card.title}</Card.Title>
+                <Card.Text>{card.description}</Card.Text>
+                <Button variant="primary">MORE</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
   );
 }
 
